dd245f5cfeb47ac8ce997c6439539a44f2613dc3
Benjamin Fleischer <work@benjaminfleischer.com>
2014-11-14 12:03:20 -0600

    Improve mysapps scripts a bit

--- my_apps.sh
+++ my_apps.sh
@@ -1,11 +1,43 @@
-find '$HOME/Library/LaunchAgents/' \( -name '*.app' -o -name '*.plist' \)
-find '$HOME/Library/Application Support/' \( -name '*.app' -o -name '*.plist' \)
-find '$HOME/Applications/' \( -name '*.app' -o -name '*.plist' \)
-find '/Library/LaunchAgents/' \( -name '*.app' -o -name '*.plist' \)
-find '/Library/LaunchDaemons' \( -name '*.app' -o -name '*.plist' \)
-find '/Library/StartupItems/' \( -name '*.app' -o -name '*.plist' \)
-find '/Library/Internet Plug-Ins/' \( -name '*.app' -o -name '*.plist' \)
-find '/Library/Application Support/' \( -name '*.app' -o -name '*.plist' \)
-find '/Applications/' \( -name '*.app' -o -name '*.plist' \)
-find '/Developer/Applications/' \( -name '*.app' -o -name '*.plist' \)
+#!/usr/bin/env bash
+function find_apps {
+  find \
+    "$HOME/Applications" \
+    "/Applications" \
+    "/Library/Developer" \
+    "/Developer/Applications" \
+    \( -name "*.app" -type d \) 2>/dev/null
+}
+function find_plists {
+  find \
+    "$HOME/Library/LaunchAgents" \
+    "$HOME/Library/Application Support" \
+    "/Library/LaunchAgents" \
+    "/Library/LaunchDaemons" \
+    "/Library/StartupItems" \
+    "/Library/Internet Plug-Ins" \
+    "/Library/Application Support" \
+    \( -name "*.plist" -type f \) 2>/dev/null
+}
 
+function find_all {
+  find_apps
+  find_plists
+}
+case "$1" in
+
+  all)
+    find_all
+  ;;
+
+  plists)
+    find_plists
+  ;;
+
+  apps)
+    find_apps
+  ;;
+
+  *)
+    echo "Usage: ./my_apps.sh [all|plists|apps]"
+  ;;
+esac
