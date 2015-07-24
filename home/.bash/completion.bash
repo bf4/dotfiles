d64da094f8b8f6c04ff400f2645e89e7799145cf
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-07-24 14:33:29 -0500

    Fallback to local git completion without Xcode.app

--- home/.bash/completion.bash
+++ home/.bash/completion.bash
@@ -11,18 +11,21 @@ fi
 # shell_name=$(echo $0 | cut -d- -f2)
 # shell_completions_file="$(brew --repository)/Library/Contributions/brew_$(echo $0 | cut -d- -f2)_completion.sh"
 # [ -s $shell_completions_file ] && source $shell_completions_file
+
+# If Commandline tools are installed without Xcode.app,
+# we'll fall back to local git completion
 # handle various places git completion may be
-# if [ -s `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
-#   echo "using homebrew git completion"
-# elif [ -f /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash ]; then
-#   # echo "using apple git completion for /usr/bin/git"
-#   source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash
-# else
-#   # fall back to local (linked) copy
-#   echo "using local git completion"
-#   source $HOME/.git-completion.sh
-#   # maybe see brew install bash-completion
-#   # source $(brew --prefix)/etc/bash_completion.d/git-prompt.sh
-# fi
+if [ -s `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
+  # echo "using homebrew git completion"
+elif [ -f /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash ]; then
+  # echo "using apple git completion for /usr/bin/git"
+  source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash
+else
+  # fall back to local (linked) copy
+  # echo "using local git completion"
+  source $HOME/.git-completion.sh
+  # maybe see brew install bash-completion
+  # source $(brew --prefix)/etc/bash_completion.d/git-prompt.sh
+fi
 # brew install grc
 [ -s "`brew --prefix grc`/etc/grc.bashrc" ] && source "`brew --prefix grc`/etc/grc.bashrc"
