d9656cb67cbdd360d00c395012882ffeb62e5cb2
Benjamin Fleischer <github@benjaminfleischer.com>
2015-08-10 23:07:37 -0500

    Add go completion

--- home/.bash/completion.bash
+++ home/.bash/completion.bash
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
+_source_if_exists() {
+  [ -f "$1" ] && source "$1"
+}
 # brew install bash_completion
 # Homebrew's own bash completion script has been installed to
 #   /usr/local/etc/bash_completion.d
 #
 #   Bash completion has been installed to:
 #     /usr/local/etc/bash_completion.d
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  source $(brew --prefix)/etc/bash_completion
-fi
+_source_if_exists "$(brew --prefix)/etc/bash_completion"
+
 # shell_name=$(echo $0 | cut -d- -f2)
 # shell_completions_file="$(brew --repository)/Library/Contributions/brew_$(echo $0 | cut -d- -f2)_completion.sh"
 # [ -s $shell_completions_file ] && source $shell_completions_file
@@ -16,7 +18,7 @@ fi
 # we'll fall back to local git completion
 # handle various places git completion may be
 # if not using homebrew git completion
-if [ ! -f $(brew --prefix)/etc/bash_completion.d/git-completion.bash ]; then
+if [ ! -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ]; then
   if [ -s /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash ]; then
     # echo "using apple git completion for /usr/bin/git"
     source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash
@@ -29,4 +31,6 @@ if [ ! -f $(brew --prefix)/etc/bash_completion.d/git-completion.bash ]; then
   fi
 fi
 # brew install grc
-[ -s "`brew --prefix grc`/etc/grc.bashrc" ] && source "`brew --prefix grc`/etc/grc.bashrc"
+_source_if_exists "$(brew --prefix grc)/etc/grc.bashrc"
+
+_source_if_exists "$(brew --prefix)/etc/bash_completion.d/go"
