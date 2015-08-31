c329793eb5fb47722c358d6aab7e9c02baa1f400
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-08-31 10:04:44 -0500

    Update brew permissions to 775 in non-dirty way

--- brew_group.sh
+++ brew_group.sh
@@ -1,5 +1,45 @@
+source ~/.shells/prompt.sh
 whoami=$(whoami)
-for lib in "/Library/Caches/Homebrew/" "/opt/homebrew-cask/" "/usr/local/"; do
-  sudo chown -R "$whoami:brew" "$lib"
-  sudo chmod -R 775 "$lib"
+pwd=$(pwd)
+set_perms() {
+  local dir="$1"
+  sudo chown -R "$whoami:brew" "$dir"
+  sudo chmod -R 775 "$dir"
+}
+update_git() {
+  local dir="$1"
+  if [ -f "$dir/.git/config" ]; then
+    cd "$dir"
+    printf "in $(pwd)"
+      find_git_status &&
+      echo $'\t'
+      printf " git status: ${git_status}"
+      git commit -am "Update permission to 775" > /dev/null
+      echo $'\t'
+      find_git_status
+      printf " git status: ${git_status}"
+    cd $pwd
+    true
+  else
+    false
+  fi
+}
+for lib in \
+  "/usr/local" \
+  "/Library/Caches/Homebrew" \
+  "/Library/Caches/Homebrew/Casks" \
+  "/opt/homebrew-cask" \
+  "/opt/homebrew-cask/Caskroom" \
+  "/usr/local/Cellar/brew-cask/$(brew cask --version)" \
+  ; do
+  printf $lib
+  set_perms "$lib"
+  update_git "$lib"
+  echo $'\n'
 done
+taps_dir="/usr/local/Library/Taps"
+find $taps_dir -maxdepth 2 -type d | while read dir; do
+  set_perms "$dir"
+  update_git "$dir" && echo $'\n'
+done
+echo $'\n'
