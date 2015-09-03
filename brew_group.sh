0b3e69349bd132568f87317681444f9774a2598f
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-09-03 09:34:47 -0500

    Avoid 775 permissions for now

--- brew_group.sh
+++ brew_group.sh
@@ -1,29 +1,47 @@
+# https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Tips-N'-Tricks.md
+# https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/FAQ.md#my-mac-apps-dont-find-usrlocalbin-utilities
 source ~/.shells/prompt.sh
 whoami=$(whoami)
 pwd=$(pwd)
 set_perms() {
   local dir="$1"
   sudo chown -R "$whoami:brew" "$dir"
-  sudo chmod -R 775 "$dir"
+  # sudo chmod -R 664 "$dir"
+  # see https://github.com/Homebrew/homebrew/issues/41942
+  #     https://github.com/Homebrew/homebrew/issues/25952
+  # 755 or 775
+  # sudo chmod -R 775 "$dir"
+}
+last_commit_message() {
+  git log -1 --pretty=%B | cat
+  # git log -1 | cat
 }
 update_git() {
   local dir="$1"
   if [ -f "$dir/.git/config" ]; then
     cd "$dir"
     printf "in $(pwd)"
+      last_commit_message
+      git remote update
+      git reset --hard origin/master
+      last_commit_message
+
       find_git_status &&
       echo $'\t'
       printf " git status: ${git_status}"
-      git commit -am "Update permission to 775" > /dev/null
-      echo $'\t'
-      find_git_status
-      printf " git status: ${git_status}"
+
+      # This causes bottles to not be used
+      # git commit -am "Update permission to 775" > /dev/null
+      # echo $'\t'
+      # find_git_status
+      # printf " git status: ${git_status}"
     cd $pwd
     true
   else
     false
   fi
 }
+echo $'\nSetting brew permissions\n'
 for lib in \
   "/usr/local" \
   "/Library/Caches/Homebrew" \
