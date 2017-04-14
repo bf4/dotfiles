d1e768d4f7f31419b4ae575ce4e864d8911e678b
Benjamin Fleischer <github@benjaminfleischer.com>
2017-04-13 21:00:39 -0500

    Update setup tmux script

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+set -eou pipefail
+
+if command -v tmux &>/dev/null; then
+  set +e
+  brew upgrade tmux
+  set -e
+else
+  brew install tmux
+fi
+
+if [ -e ~/.tmux/plugins/tpm ]; then
+  unset CDPATH
+  cd ~/.tmux/plugins/tpm
+  git remote update && git pull --rebase
+else
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
