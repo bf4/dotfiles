1ecda446d44c80cf701e1b4bb354a4abedd8b811
Benjamin Fleischer <github@benjaminfleischer.com>
2020-03-09 15:47:26 -0500

    Show git unmerged branch history

--- home/.bash/misc.sh
+++ home/.bash/misc.sh
@@ -3,3 +3,16 @@
 shopt -s checkwinsize
 
 alias peek='tee >(cat 1>&2)'
+
+# git-branch-history() {
+#   git reflog | grep 'checkout: moving' | head -20 | awk '{ print $6,\"\t\t\t\t\",$8 }'
+# }
+git-unmerged-branch-history() {
+  comm -12 <(git branch | awk '{ print $1 }' | sort) <(
+    comm -23 <(
+      git reflog | grep 'checkout: moving' | head -100 | awk '{ print $8 }' | sort -u | grep -v master
+    ) <(
+      git branch master --no-merged | sort
+    )
+  )
+}
